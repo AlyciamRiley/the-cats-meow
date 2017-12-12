becaf430d2dc9fe3a2ec2801df7616abd3e38c45
Alycia Riley <alyciadahlenm@gmail.com>
2017-12-11 19:13:43 -0600

    code what you know.

---
+++ assets/javascript/game.js
@@ -0,0 +1,44 @@
+//Global Variables
+//=========================================================
+
+var winCounter = 0;
+var lossCounter = 0;
+var totalScoure = 0;
+var targetNumber = 0;
+var pirateCat = 0;
+var watermelonCat = 0;
+var boaCat = 0;
+var bunnyCat = 0;
+
+
+//start of game
+//===========================================================
+
+//computer generates random "goal" number
+
+//computer randomly assigns values to each cat
+
+//total score is set to zero
+
+//wins and losses are set to zero
+
+
+//user interaction
+//============================================================
+
+//user presses crystal (on click event)
+
+//number is added to "total score" box
+
+//user continues to choose crystals until target number is met
+
+//If target number is met..
+        
+    //win counter goes up by 1
+    
+    //game starts over
+
+//if total score exceeds target number
+    
+    //losses goes up by 1
+
