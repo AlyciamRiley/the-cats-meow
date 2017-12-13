c0071ae885056af05e44d49c7213c604b695b5da
unknown <alyciarileym@gmail.com>
2017-12-12 19:53:25 -0600

    getting closer.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,13 +3,13 @@
 
 var winCounter = 0;
 var lossCounter = 0;
-var totalScore = 0;
-var targetNumber = [];
+var totalScore = '';
+var targetNumber;
 var pirateCat = 0;
 var watermelonCat = 0;
 var boaCat = 0;
 var bunnyCat = 0;
-//var computerGuess= [];
+
 
 
 //start of game
@@ -18,7 +18,7 @@ function startGame() {
     //computer generates random "goal" number
     targetNumber = Math.floor(Math.random() * 99) + 1;
     targetNumber = $("#target-number").text(targetNumber);
-    
+
 
 
 
@@ -37,7 +37,7 @@ function startGame() {
     //console.log(bunnyCat);
 
     //total score is set to zero
-    totalScore = [];
+    
 
     //wins and losses are set to zero
 
@@ -49,55 +49,65 @@ startGame();
 //user interaction
 //============================================================
 function gamePlay() {
-    
 
+   $('#total-score').text("Total Score: " + totalScore);
     //user presses bunny cat (on click event)
-    $("#bunnyCat").on("click", function () {
-        totalScore += bunnyCat;
+    $("#bunnyCat").click(function () {
         //number is added to total score
-        //totalScore = $("#total-score").replaceWith(totalScore);
-
+        totalScore = Number(totalScore) + Number($(this).val());
+        $('#total-score').text("Total Score: " + totalScore);
+        totalScore += bunnyCat;
+        
+        
         console.log(totalScore);
     });
 
-    
-      //user presses boa cat (on click event)
-    $("#boaCat").on("click", function () {
+
+
+    //user presses boa cat (on click event)
+    $("#boaCat").click(function () {
+        //number is added to total score
+        totalScore = Number(totalScore) + Number($(this).val());
+        $('#total-score').text("Total Score: " + totalScore);
         totalScore += boaCat;
-       //totalScore = $("#total-score").replaceWith(totalScore);
-        console.log(totalScore);
+
+      console.log(totalScore);
     });
-    
-      //user presses pirate cat (on click event)
-    $("#pirateCat").on("click", function () {
+
+    //user presses pirate cat (on click event)
+    $("#pirateCat").click(function () {
+        //number is added to total score
+        totalScore = Number(totalScore) + Number($(this).val());
+        $('#total-score').text("Total Score: " + totalScore);
         totalScore += pirateCat;
-      // totalScore = $("#total-score").replaceWith(totalScore);;
+        
         console.log(totalScore);
     });
 
-      //user presses watermelon cat (on click event)
-    $("#watermelonCat").on("click", function () {
+
+    //user presses watermelon cat (on click event)
+    $("#watermelonCat").click(function () {
+        //number is added to total score
+        totalScore = Number(totalScore) + Number($(this).val());
+        $('#total-score').text("Total Score: " + totalScore);
         totalScore += watermelonCat;
-        //totalScore = $("#total-score").replaceWith(totalScore);
+       
         console.log(totalScore);
     });
-    
-     
 
-//number is added to "total score" box
 
     //user continues to choose crystals until target number is met
 
     //If target number is met..
     if (totalScore === targetNumber) {
-         //win counter goes up by 1
+        //win counter goes up by 1
         wins++
         alert("you win");
         //game starts over
         startGame();
-        
+
     }
-   
+
     //if total score exceeds target number
     if (totalScore > targetNumber) {
         //losses goes up by 1
@@ -106,19 +116,15 @@ function gamePlay() {
         //game starts over
         startGame();
     }
-    
-  
 
-    
 
-    
 
-    
 
-    
-  
+
+
+
+
 
 }
 
 gamePlay();
-
